c27e9211316c5588f9ac2bf2a871b339d3257a4f
Giuseppe Formisano <giuseppe.a.formisano@accenture.com>
2026-08-18 20:50:46 +0200

    Bump 0.12.3-beta (versionCode 30)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.familyradar.rkwvpm"
     minSdk = 24
     targetSdk = 36
-    versionCode = 29
-    versionName = "0.12.2-beta"
+    versionCode = 30
+    versionName = "0.12.3-beta"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }

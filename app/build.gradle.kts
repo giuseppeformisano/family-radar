83290e6f4cdd20d4c41084d0f1a3708245fcc04a
Giuseppe Formisano <tonybanks989@gmail.com>
2026-08-22 17:11:19 +0200

    chore: bump version to 0.12.47-beta (versionCode 74)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.familyradar.rkwvpm"
     minSdk = 24
     targetSdk = 36
-    versionCode = 71
-    versionName = "0.12.44-beta"
+    versionCode = 74
+    versionName = "0.12.47-beta"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
